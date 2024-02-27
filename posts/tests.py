8b064ce83d951e9b075eea0839e2a9c41874ba88
sam2829 scarisbrick <samscarisbrick@outlook.com>
2024-02-27 12:46:30 +0000

    update and ad new tests for post view tests

--- posts/tests.py
+++ posts/tests.py
@@ -64,21 +64,45 @@ class PostListViewTests(APITestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
+class PostDetailViewTests(APITestCase):
+    """
+    this class is for testing the Post Detail views
+    """
+    # setup logged in users
+    def setUp(self):
+        sam = User.objects.create_user(username='sam', password='pass')
+        emma = User.objects.create_user(username='emma', password='pass')
+        Posts.objects.create(owner=sam, title='test title', content='sam content')
+        Posts.objects.create(owner=emma, title='another title', content='emma content')
+
+
+    # create a image for testing
+    def generate_image(self):
+        file = io.BytesIO()
+        image = Image.new('RGBA', size=(100, 100), color=(0, 255, 0, 255))  # Green color with full opacity
+        image.save(file, 'png')
+        file.name = 'test.png'
+        file.seek(0)
+        return file
+
     # test that user can retrieve single post by id
     def test_can_retrieve_post_detail(self):
-        sam = User.objects.get(username='sam')
-        post = Posts.objects.create(owner=sam, title='test title', content='test content')
-        response = self.client.get(f'/posts/{post.id}/')
+        response = self.client.get(f'/posts/1/')
+        self.assertEqual(response.data['title'], 'test title')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    # test user cannot retrieve a post with invalid id
+    def test_cannot_retrieve_post_detail_using_invalid_id(self):
+        response = self.client.get(f'/posts/23/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
 
     # test that user can update a post
     def test_logged_in_user_can_update_post(self):
-        sam = User.objects.get(username='sam')
         image = self.generate_image()
-        post = Posts.objects.create(owner=sam, title='test title', content='test content')
         self.client.login(username='sam', password='pass')
         response = self.client.put(
-            f'/posts/{post.id}/',
+            '/posts/1/',
             {
                 'title': 'updated title',
                 'character_name': 'test character',
@@ -87,15 +111,32 @@ class PostListViewTests(APITestCase):
                 'image': image,
             }
         )
+        post = Posts.objects.filter(pk=1).first()
+        self.assertEqual(post.title, 'updated title')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    # test that user cannot updated someone elses post
+    def test_logged_in_user_cannot_update_anothers_post(self):
+        image = self.generate_image()
+        self.client.login(username='sam', password='pass')
+        response = self.client.put(
+            '/posts/2/',
+            {
+                'title': 'updated title',
+                'character_name': 'test character',
+                'character_category': 'Avenger',
+                'content': 'updated content',
+                'image': image,
+            }
+        )
+        post = Posts.objects.filter(pk=2).first()
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     # test that user cannot update post logged out
     def test_logged_out_user_cannot_update_post(self):
-        sam = User.objects.get(username='sam')
         image = self.generate_image()
-        post = Posts.objects.create(owner=sam, title='test title', content='test content')
         response = self.client.put(
-            f'/posts/{post.id}/',
+            f'/posts/1/',
             {
                 'title': 'updated title',
                 'character_name': 'test character',
@@ -104,22 +145,26 @@ class PostListViewTests(APITestCase):
                 'image': image,
             }
         )
+        post = Posts.objects.filter(pk=1).first()
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     # test that user can delete a post
     def test_logged_in_user_can_delete_post(self):
-        sam = User.objects.get(username='sam')
-        post = Posts.objects.create(owner=sam, title='test title', content='test content')
         self.client.login(username='sam', password='pass')
-        response = self.client.delete(f'/posts/{post.id}/')
+        response = self.client.delete(f'/posts/1/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     # test that user cannot delete someone elses post
     def test_unauthorized_user_cannot_delete_post(self):
-        sam = User.objects.get(username='sam')
-        post = Posts.objects.create(owner=sam, title='test title', content='test content')
-        response = self.client.delete(f'/posts/{post.id}/')
+        self.client.login(username='sam', password='pass')
+        response = self.client.delete(f'/posts/2/')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    # test that user cannot delete a post logged out
+    def test_logged_out_user_cannot_delete_post(self):
+        response = self.client.delete(f'/posts/2/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
 
     # test invalid data in post is not accepted
     def test_invalid_post_data(self):
