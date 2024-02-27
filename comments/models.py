9e4750c149f1b90dd0c827693e7249958b0a3de0
sam2829 scarisbrick <samscarisbrick@outlook.com>
2024-02-27 12:55:53 +0000

    Add comments app, create comments model and create comments admin.py

---
+++ comments/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Posts
+
+
+class Comment(models.Model):
+    """
+    this model is to be related to the 'owner',
+    'Posts' and is for user comments
+    """
+    
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    agree = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.content
