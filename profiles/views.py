9e982496b8263f6009e0e51b27d5b220d5675dda
sam2829 scarisbrick <samscarisbrick@outlook.com>
2024-02-28 11:04:12 +0000

    Add annotate for posts count, followers count and following count in profiles views.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
 from strongest_avenger_drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -9,8 +10,22 @@ class ProfileList(generics.ListAPIView):
     """
     This class is to render the list of all profiles
     """
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__posts', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner_followed_created_at',
+    ]
     
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
@@ -20,4 +35,8 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__posts', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
