c9eb20d068417a0da6d1e15092ad4f75d9e18ca1
sam2829 scarisbrick <samscarisbrick@outlook.com>
2024-02-28 12:29:09 +0000

    Add searh filters for posts and profile. install django-filter

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from strongest_avenger_drf_api.permissions import IsOwnerOrReadOnly
 from .models import Posts
 from .serializers import PostsSerializer
@@ -20,6 +21,13 @@ class PostList(generics.ListCreateAPIView):
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'likes__owner__profile',
+        'owner__profile',
+        'character_category',
     ]
     search_fields = [
         'owner__username',
