6c654dbf5047329fe8ede37185f73333e4f96760
sam2829 scarisbrick <samscarisbrick@outlook.com>
2024-03-30 15:12:21 +0000

    Add quertset to report create views

--- report/views.py
+++ report/views.py
@@ -9,6 +9,7 @@ class ReportCreate(generics.ListCreateAPIView):
     """
     serializer_class = ReportSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Report.objects.all() 
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
